6bcc44e5808fee445e76d8523784bebbada3392f
Duncan Campbell <dac80@zips.uakron.edu>
2016-10-04 00:30:47 -0400

    Indent assblembly language correctly...grr...for some reason the assembler must have all comments to the left with all instructions indented....

--- Pong/Pong.asm
+++ Pong/Pong.asm
@@ -1,119 +1,118 @@
-processor 6502
-include "vcs.h"
-include "macro.h"
+    processor 6502
+    include "vcs.h"
+    include "macro.h"
 
-SEG
-ORG $F000
+    SEG
+    ORG $F000
 
 Reset
 
 StartOfFrame
 
 ; Start of vertical blank processing
-lda #0
-sta VBLANK
+    lda #0
+    sta VBLANK
 
-lda #2
-sta VSYNC
+    lda #2
+    sta VSYNC
 
 ; 3 scanlines of VSYNCH signal...
-sta WSYNC
-sta WSYNC
-sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    lda #0
 
-lda #0
-
-sta VSYNC
+    sta VSYNC
 
 ; 37 scanlines of vertical blank...
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
 
 ; 192 scanlines of picture...
-ldx #0
-REPEAT 192; scanlines
-    inx
-    stx COLUBK
-    sta WSYNC
-REPEND
+    ldx #0
+    REPEAT 192; scanlines
+        inx
+        stx COLUBK
+        sta WSYNC
+    REPEND
 
-lda #%01000010
+    lda #%01000010
 
-sta VBLANK                     ; end of screen - enter blanking
+    sta VBLANK                     ; end of screen - enter blanking
 
 ; 30 scanlines of overscan...
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
-sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
 
-jmp StartOfFrame
+    jmp StartOfFrame
 
-ORG $FFFA
+    ORG $FFFA
 
-.word Reset          ; NMI
-.word Reset          ; RESET
-.word Reset          ; IRQ
+    .word Reset          ; NMI
+    .word Reset          ; RESET
+    .word Reset          ; IRQ
 
-END
+    END
